cfb40d3f42748ce26f8018b30d3d0c9d0d5ea5af
Sanjar <sanjarrr28@gmail.com>
2024-09-15 00:55:23 +0200

    last change rack app

--- app.rb
+++ app.rb
@@ -1,21 +1,35 @@
 require_relative 'middleware/time_formatter'
+require 'byebug'
 
 class App
   def call(env)
     req = Rack::Request.new(env)
     format = req.params['format']
 
-    if req.path == "/time" && format
+    build_response(req.path, format)
+  end
+
+  def build_response(path, format)
+    response = Rack::Response.new
+
+    if path == '/time' && format
       time_formatter = TimeFormatter.new(format)
 
-      time_formatter.response
+      if time_formatter.unknown_formats.any?
+        response.status = 400
+        response.headers['Content-Type'] = 'text/plain'
+        response.write("Unknown time format [#{time_formatter.unknown_formats.join(', ')}]")
+      else
+        response.status = 200
+        response.header['Content-Type'] = 'text/plain'
+        response.write(time_formatter.time)
+      end
     else
-      res = Rack::Response.new
-      res.status = 404
-      res.header['Content-Type'] = 'text/plain'
-      res.write('Not Found')
-
-      res.finish
+      response.status = 404
+      response.header['Content-Type'] = 'text/plain'
+      response.write('Not Found')
     end
+
+    response.finish
   end
 end
--- middleware/time_formatter.rb
+++ middleware/time_formatter.rb
@@ -1,32 +1,29 @@
 class TimeFormatter
-  SUPPORTED_FORMATS = { 'year' => '%Y', 'month' => '%m', 'day' => '%d', 'hour' => '%H', 'minute' => '%M',
-                        'second' => '%S' }.freeze
+  SUPPORTED_FORMATS = {
+                        'year' => '%Y', 'month' => '%m', 'day' => '%d',
+                        'hour' => '%H', 'minute' => '%M', 'second' => '%S'
+                      }.freeze
+
+  ORDERED_FORMATS = %w[year month day hour minute second].freeze
+
   def initialize(format)
     @format = format.split(',')
   end
 
   def time
-    time_format = @format.map { |part| SUPPORTED_FORMATS[part] }.join('-')
-    Time.now.strftime(time_format)
+    date_format = ORDERED_FORMATS.select { |part| @format.include?(part) && %w[year month day].include?(part) }
+                                 .map { |part| SUPPORTED_FORMATS[part] }.compact.join('-')
+
+    time_format = ORDERED_FORMATS.select { |part| @format.include?(part) && %w[hour minute second].include?(part) }
+                                 .map { |part| SUPPORTED_FORMATS[part] }.compact.join(':')
+
+    format_string = [date_format, time_format].reject(&:nil?).join(' ')
+
+    Time.now.strftime(format_string)
   end
 
   def unknown_formats
     @format - SUPPORTED_FORMATS.keys
   end
 
-  def response
-    response = Rack::Response.new
-
-    if unknown_formats.any?
-      response.status = 400
-      response.headers['Content-Type'] = 'text/plain'
-      response.write("Unknown time format [#{unknown_formats.join(', ')}]")
-    else
-      response.status = 200
-      response.header['Content-Type'] = 'text/plain'
-      response.write(time)
-    end
-
-    response.finish
-  end
 end
