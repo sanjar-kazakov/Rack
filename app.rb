7c8ae2225d497adbffa1b2d5f6afc57ae78d7c4e
Sanjar <sanjarrr28@gmail.com>
2024-09-15 17:31:52 +0200

    the latest change rack app

--- app.rb
+++ app.rb
@@ -10,25 +10,28 @@ class App
   end
 
   def build_response(path, format)
-    response = Rack::Response.new
+
 
     if path == '/time' && format
       time_formatter = TimeFormatter.new(format)
 
       if time_formatter.unknown_formats.any?
-        response.status = 400
-        response.headers['Content-Type'] = 'text/plain'
-        response.write("Unknown time format [#{time_formatter.unknown_formats.join(', ')}]")
+        test_method(400, "Unknown time format [#{time_formatter.unknown_formats.join(', ')}]")
       else
-        response.status = 200
-        response.header['Content-Type'] = 'text/plain'
-        response.write(time_formatter.time)
+        test_method(200, time_formatter.time)
       end
     else
-      response.status = 404
-      response.header['Content-Type'] = 'text/plain'
-      response.write('Not Found')
+      test_method(404, 'Not Found')
     end
+
+
+  end
+
+  def test_method(status, body)
+    response = Rack::Response.new
+      response.status = status
+      response.write(body)
+      response.headers['Content-Type'] = 'text/plain'
 
     response.finish
   end
